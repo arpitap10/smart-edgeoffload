af72fe63d0d0f3f15e967c37c87b770c785296da
anvitaadali01 <anvitaa.is23@bmsce.ac.in>
2026-03-24 22:01:51 +0530

    Implement send_to_cloud method in CloudAPI
    Implemented the send_to_cloud method to offload tasks to a cloud service.

--- cloud/cloud_api.py
+++ cloud/cloud_api.py
@@ -1,10 +1,22 @@
+import requests
 from shared.data_models import IoTTask, ExecutionResult
 
 
 class CloudAPI:
 
     def send_to_cloud(self, task: IoTTask) -> ExecutionResult:
-        """
-        Offload task to cloud.
-        """
-        raise NotImplementedError
+
+        url = "http://13.60.34.204:8000/execute_task"
+
+        # convert task object to dictionary
+        task_data = task.__dict__
+
+        response = requests.post(url, json=task_data)
+
+        result = response.json()
+
+        return ExecutionResult(
+            status=result.get("status", "completed"),
+            location=result.get("location", "cloud"),
+            latency=result.get("latency", 0.0)
+        )
